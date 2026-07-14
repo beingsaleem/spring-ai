ff25e6fc3ff354e6590fe394dd07a1ff6aa7558a
saleem_basha <saleemlbk666@gmail.com>
2026-07-14 07:27:14 +0400

    structured output converter

--- chat-memory/src/main/java/com/chat/memory/web/MovieController.java
+++ chat-memory/src/main/java/com/chat/memory/web/MovieController.java
@@ -1,11 +1,17 @@
 package com.chat.memory.web;
 
+import com.chat.memory.Movie;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.ai.converter.ListOutputConverter;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movie")
@@ -19,8 +25,28 @@ public class MovieController {
     }
 
     @GetMapping
-    public String getMovieInfo(@RequestParam String movieName) {
+    public String getMovie(@RequestParam String movieName) {
         String prompt = "Tell me about the movie " + movieName;
         return chatClient.prompt(prompt).call().content();
+    }
+
+    @GetMapping("/info")
+    public Movie getMovieInfo(@RequestParam String movieName) {
+        return chatClient.prompt()
+                .user(u -> u.text("Give me details about the movie {movie}")
+                        .param("movie", movieName))
+                .call()
+                .entity(new BeanOutputConverter<Movie>(Movie.class));
+    }
+
+    @GetMapping("/get-movies")
+    public List<String> getDirectorMovie(@RequestParam String directorName) {
+        String prompt = "List movies directed by " + directorName;
+        return chatClient
+                .prompt()
+                .user("List movies directed by " + directorName)
+                .call()
+                .entity(new ListOutputConverter(new DefaultConversionService()));
+
     }
 }
