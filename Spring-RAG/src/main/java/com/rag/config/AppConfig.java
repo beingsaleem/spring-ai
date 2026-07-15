8aced61fb6422e57d566c226dcccbc97799d011a
saleem_basha <saleemlbk666@gmail.com>
2026-07-15 20:19:43 +0400

    pgvector to store embeddings

--- Spring-RAG/src/main/java/com/rag/config/AppConfig.java
+++ Spring-RAG/src/main/java/com/rag/config/AppConfig.java
@@ -3,14 +3,31 @@ package com.rag.config;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class AppConfig {
 
+
+//    @Bean
+//    public VectorStore simpleVectorStore(
+//            EmbeddingModel embeddingModel) {
+//
+//        return SimpleVectorStore.builder(embeddingModel).build();
+//    }
+
     @Bean
-    public VectorStore vectorStore(EmbeddingModel embeddingModel) {
-        return SimpleVectorStore.builder(embeddingModel).build();
+    public VectorStore pgVectorStore(
+            JdbcTemplate jdbcTemplate,
+            EmbeddingModel embeddingModel) {
+
+        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
+                .dimensions(1536)
+                .distanceType(PgVectorStore.PgDistanceType.COSINE_DISTANCE)
+                .indexType(PgVectorStore.PgIndexType.HNSW)
+                .build();
     }
 }
--- Spring-RAG/src/main/java/com/rag/utils/DataInitializer.java
+++ Spring-RAG/src/main/java/com/rag/utils/DataInitializer.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.TextReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -19,6 +20,16 @@ public class DataInitializer {
 
     @PostConstruct
     public void initializeData() {
+
+        List<Document> existingDocuments = vectorStore.similaritySearch(
+                SearchRequest.builder().query("myLearn").topK(1).build()
+        );
+        if (!existingDocuments.isEmpty()) {
+            System.out.println("Data already initialized. Skipping initialization.");
+            return;
+        }
+
+        System.out.println("Initializing data...");
         TextReader textReader = new TextReader(new ClassPathResource("courses.txt"));
         TokenTextSplitter tokenTextSplitter = TokenTextSplitter.builder().withChunkSize(500).build();
         List<Document> documents = tokenTextSplitter.split(textReader.get());
