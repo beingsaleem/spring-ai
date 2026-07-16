16afeca02f27327cdb8373795e607b6c1adf5654
saleem_basha <saleemlbk666@gmail.com>
2026-07-16 06:57:55 +0400

    redis vector store

--- Spring-RAG/src/main/java/com/rag/config/AppConfig.java
+++ Spring-RAG/src/main/java/com/rag/config/AppConfig.java
@@ -4,9 +4,12 @@ import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import redis.clients.jedis.JedisPooled;
 
 @Configuration
 public class AppConfig {
@@ -19,15 +22,39 @@ public class AppConfig {
 //        return SimpleVectorStore.builder(embeddingModel).build();
 //    }
 
-    @Bean
-    public VectorStore pgVectorStore(
-            JdbcTemplate jdbcTemplate,
-            EmbeddingModel embeddingModel) {
+//    @Bean
+//    public VectorStore pgVectorStore(
+//            JdbcTemplate jdbcTemplate,
+//            EmbeddingModel embeddingModel) {
+//
+//        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
+//                .dimensions(1536)
+//                .distanceType(PgVectorStore.PgDistanceType.COSINE_DISTANCE)
+//                .indexType(PgVectorStore.PgIndexType.HNSW)
+//                .build();
+//    }
 
-        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
-                .dimensions(1536)
-                .distanceType(PgVectorStore.PgDistanceType.COSINE_DISTANCE)
-                .indexType(PgVectorStore.PgIndexType.HNSW)
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+    @Value("${spring.ai.vectorstore.redis.index-name}")
+    private String indexName;
+    @Value("${spring.ai.vectorstore.redis.prefix}")
+    private String prefix;
+
+    @Bean
+    public JedisPooled jedisPooled() {
+        return new JedisPooled(redisHost, redisPort);
+    }
+
+    @Bean
+    public VectorStore redisVectorStore(JedisPooled jedisPooled, EmbeddingModel
+            embeddingModel) {
+        return RedisVectorStore.builder(jedisPooled, embeddingModel)
+                .indexName(indexName)
+                .prefix(prefix)
+                .initializeSchema(true) // auto create the search index on startup
                 .build();
     }
 }
